ea5d4a87dabd6a4445d15b51ee3a493ad66696c0
Shane Marcotte <shanemarcotte@gmail.com>
2020-04-20 15:27:27 -0400

    Add files via upload

---
+++ xandercage.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "You're a jewel thief trying to disable a museum security system...\n";
+	std::cout << "You will need to enter a series of codes to turn off the laser grid in each section...\n";
+
+	const int a = 4;
+	const int b = 3;
+	const int c = 2;
+
+	const int sum = a + b + c;
+	const int product = a * b * c;
+
+	std::cout << sum << "\n";
+	std::cout << product << "\n";
+
+	return 0;
+}
